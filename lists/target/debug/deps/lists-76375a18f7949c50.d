2d54046680a3faf7b98748bef28f6c8cb8bb65d1
duanjr <duanjr02@gmail.com>
2025-01-18 05:12:38 -0800

    implement easist list

---
+++ lists/target/debug/deps/lists-76375a18f7949c50.d
@@ -0,0 +1,8 @@
+/home/duanjr/learn_rust/lists/target/debug/deps/liblists-76375a18f7949c50.rmeta: src/lib.rs src/first.rs
+
+/home/duanjr/learn_rust/lists/target/debug/deps/liblists-76375a18f7949c50.rlib: src/lib.rs src/first.rs
+
+/home/duanjr/learn_rust/lists/target/debug/deps/lists-76375a18f7949c50.d: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
---
+++ lists/target/debug/liblists.d
@@ -0,0 +1,1 @@
+/home/duanjr/learn_rust/lists/target/debug/liblists.rlib: /home/duanjr/learn_rust/lists/src/first.rs /home/duanjr/learn_rust/lists/src/lib.rs
